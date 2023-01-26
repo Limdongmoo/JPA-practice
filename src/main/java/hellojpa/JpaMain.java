7f2018ec4feec2f03f27648760d3f409c3d9c5ca
Limjongho <whdgh9873@icloud.com>
2023-01-26 12:26:59 +0900

    JPQL 조회 + 페이징

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,9 +16,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class,1L);
-            findMember.setName("nameChange");
-            tx.commit();
+//            Member findMember = em.find(Member.class,1L);
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(1)      //페이징 최소
+                    .setMaxResults(10)      //페이징 최대
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
 
         } catch (Exception e) {
             tx.rollback();
