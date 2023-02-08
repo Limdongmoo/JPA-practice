197f4d4b33c9df63305ae755e550dbe97b258335
Limjongho <whdgh9873@icloud.com>
2023-02-08 16:27:49 +0900

    JPA 5장 학습 완료

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,9 +5,15 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "member_seq_generator",
+        sequenceName = "member_seq",
+        initialValue = 1, allocationSize = 50
+)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
     private Integer age;
@@ -24,4 +30,12 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private RoleType roleType;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
